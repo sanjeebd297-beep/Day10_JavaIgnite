4e250a8d9305ed56103758f761b4ecbf1c4b062b
sanjeeb 25bcsa50 <sanjeebd297@gmail.com>
2026-06-06 08:15:03 +0530

    Implement ATM withdrawal logic with error handling
    Added input validation for withdrawal amount and balance.

--- ATM_check.java
+++ ATM_check.java
@@ -11,3 +11,35 @@ If input is invalid (text instead of number) → handle exception
 Expected Output:
 Transaction Successful / Insufficient Balance / Invalid Input
 */
+package JavaIgniteDay10;
+import java.util.Scanner;
+
+public class ATM_Check_System {
+	public static void main(String[] argd) {
+		
+	Scanner sc = new Scanner(System.in);
+	int balance,withdrawl,remain;
+	try {
+		System.out.println("Enter Balance :");
+		balance = sc.nextInt();
+		
+		System.out.println("Enter Withdrawl Amount :");
+		withdrawl = sc.nextInt();
+		if(withdrawl > balance )
+		{
+			System.out.println("Invalid input");
+		}
+		if(withdrawl < 0 )
+		{
+			System.out.println("Withdrawl amount can't be negative");
+		}
+		remain = balance - withdrawl;
+		System.out.println("Withdrawn Sucessfully \nRemaining Balance : "+ remain);
+	}
+	catch(Exception e) 
+	{
+		System.out.println("INVALID INPUT...PLEASE ENTER A VALID NUMBER");
+	}
+	sc.close();
+	}
+}
