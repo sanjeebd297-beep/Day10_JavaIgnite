2eff04c369ad8991129941a43f3215ffbdcd3762
sanjeeb 25bcsa50 <sanjeebd297@gmail.com>
2026-06-06 08:16:31 +0530

    Implement string processing with index handling
    Added user input handling for string processing and exception management.

--- String_Processor.java
+++ String_Processor.java
@@ -17,3 +17,37 @@ StringIndexOutOfBoundsException
 Example:
 Invalid index handled safely
 */
+package JavaIgniteDay10;
+import java.util.Scanner;
+
+public class String_Processor {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		String str;
+		System.out.println("Enter a String");
+		str = sc.nextLine();
+		
+		System.out.println("Main is "+ str);
+		
+		try
+		{
+			System.out.println("Enter the index for substring(beg index / end index) ");
+			int beg = sc.nextInt();
+			int end = sc.nextInt();
+			System.out.println(str.substring(beg,end));
+			System.out.println("Enter the index you want to view :");
+			int index = sc.nextInt();
+			System.out.println(str.charAt(index));
+		}
+		
+		catch(StringIndexOutOfBoundsException e)
+		{
+			System.out.println("In valid index");
+		}
+		sc.close();
+	}
+	
+
+}
